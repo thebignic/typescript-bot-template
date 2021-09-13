5e7b49a7bcf779918be2b96a3ed8cd28a7180545
thebignic <nicolascooker@gmail.com>
2021-09-12 21:02:12 -0500

    client permission checks in InteractionCreateEvent.ts as well as instant defer

--- src/bot/events/InteractionCreateEvent.ts
+++ src/bot/events/InteractionCreateEvent.ts
@@ -11,6 +11,7 @@ export default abstract class InteractionCreateEvent extends Event {
 
   async exec(interaction: Interaction) {
     if (interaction.isCommand()) {
+      await interaction.deferReply({ ephemeral: true });
       const command = this.client.commands.get(interaction.commandName);
       if (command) {
         if (
@@ -21,6 +22,17 @@ export default abstract class InteractionCreateEvent extends Event {
             content: 'This command can only be used by the owner of the bot.',
             ephemeral: true,
           });
+        const clientPermissions = command.clientPermissions;
+        const missingPermissions = [];
+        if (clientPermissions?.length)
+          for (let i: number = 0; i < clientPermissions.length; i++) {
+            const hasPermission = interaction.guild?.me?.permissions.has(
+              clientPermissions[i]
+            );
+            if (!hasPermission) missingPermissions.push(clientPermissions[i]);
+          }
+        if (missingPermissions.length) return interaction.deferReply();
+
         command.exec(interaction);
       }
     }
