3776ac7c3fd826b64a7c9fc78b57a25f3cc10629
thebignic <nicolascooker@gmail.com>
2021-09-13 14:30:00 -0500

    finished InteractionCreateEvent.ts by adding cooldown functionality

--- src/bot/events/InteractionCreateEvent.ts
+++ src/bot/events/InteractionCreateEvent.ts
@@ -1,6 +1,7 @@
 import Event from '../struct/Event';
-import { Interaction } from 'discord.js';
+import { Collection, Interaction } from 'discord.js';
 import settings from '../settings';
+import Embed from '../struct/Embed';
 
 export default abstract class InteractionCreateEvent extends Event {
   protected constructor() {
@@ -9,7 +10,7 @@ export default abstract class InteractionCreateEvent extends Event {
     });
   }
 
-  async exec(interaction: Interaction) {
+  async exec(interaction: Interaction): Promise<void> {
     if (interaction.isCommand()) {
       await interaction.deferReply({ ephemeral: true });
       const command = this.client.commands.get(interaction.commandName);
@@ -32,8 +33,51 @@ export default abstract class InteractionCreateEvent extends Event {
             if (!hasPermission) missingPermissions.push(clientPermissions[i]);
           }
         if (missingPermissions.length) return interaction.deferReply();
-
-        command.exec(interaction);
+        if (command.cooldown) {
+          if (!this.client.cooldowns.has(command.name)) {
+            this.client.cooldowns.set(command.name, new Collection());
+          }
+          const now = Date.now();
+          const timestamps = this.client.cooldowns.get(command.name);
+          const cooldownAmount = command.cooldown * 1000;
+          if (timestamps?.has(interaction.user.id)) {
+            const cooldown = timestamps.get(interaction.user.id);
+            if (cooldown) {
+              const expirationTime = cooldown + cooldownAmount;
+              if (now < expirationTime)
+                return interaction.reply({
+                  embeds: [
+                    new Embed().setBase(
+                      'Command on Cooldown',
+                      `You'll be able to use this command again in \`${(
+                        (expirationTime - now) /
+                        1000
+                      ).toFixed(1)}s\`.`
+                    ),
+                  ],
+                  ephemeral: true,
+                });
+            }
+          }
+          timestamps?.set(interaction.user.id, now);
+          setTimeout(
+            () => timestamps?.delete(interaction.user.id),
+            cooldownAmount
+          );
+        }
+        try {
+          await command.exec(interaction);
+          return;
+        } catch {
+          await interaction.reply({
+            embeds: [
+              new Embed().setError(
+                'There was an error executing this command.'
+              ),
+            ],
+          });
+          return;
+        }
       }
     }
   }
